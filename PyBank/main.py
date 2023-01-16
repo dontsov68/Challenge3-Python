fcf319aef6f59badd4f34350c9139c449970d50f
Victor Dontsov <dontsov68@gmail.com>
2023-01-15 23:32:20 -0500

    Initialization of git repo, added readme file

---
+++ PyBank/main.py
@@ -0,0 +1,43 @@
+import os
+import csv
+results={}
+differ={}
+bank = os.path.join("..", "PyBank", "Resources", "budget_data.csv")
+with open(bank, encoding="utf-8") as file:
+    reader=csv.DictReader(file, delimiter=",")
+    print("")
+    print('Financial Analysis')
+    print("---------------------------------------")
+    p=0
+    max=0
+    min=0
+    sum=0
+    results[0]=0
+    for row in reader:
+         results[p+1]=row["Profit/Losses"]
+         delta=float(results[p+1])-float(results[p])
+         if delta > max:
+          max=delta
+          pmax=row["Date"]
+         if delta < min:
+          min=delta
+          pmin=row["Date"] 
+         sum+=int(row["Profit/Losses"])
+         p+=1
+    dif=round((float(results[p])-float(results[1]))/(p-1),ndigits=2)
+    print(f'Total Months: {reader.line_num-1}')
+    print(f'Total Amount: ${sum}')
+    print(f'Average Change: ${dif}')
+    print(f'Greatest Increase Change: {pmax} (${max})')
+    print(f'Greatest Decrease Change: {pmin} (${min})')
+        
+
+fu=open('analysis\pybank.txt', 'w')
+fu.write('Financial Analysis'+ '\n')
+fu.write("---------------------------------------"+ '\n')
+fu.write(f'Total Months: {reader.line_num-1}'+ '\n')
+fu.write(f'Total Amount: ${sum}'+ '\n')
+fu.write(f'Average Change: ${dif}'+ '\n')
+fu.write(f'Greatest Increase Change: {pmax} (${max})'+ '\n')
+fu.write(f'Greatest Decrease Change: {pmin} (${min})'+ '\n')
+fu.close()
