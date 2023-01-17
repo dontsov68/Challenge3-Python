4d2ec6ee26ae5a55fdc2cf9b6802e57d3dc1ba77
Victor Dontsov <dontsov68@gmail.com>
2023-01-17 00:03:21 -0500

    monday commit

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -1,45 +1,48 @@
 import os
 import csv
 results={}
-differ={}
 bank = os.path.join("..", "PyRoll", "Resources", "election_data.csv")
 with open(bank, encoding="utf-8") as file:
     reader=csv.DictReader(file, delimiter=",")
     print("")
     print('Election Results')
     print("---------------------------------------")
-    p=0
-    max=0
-    min=0
-    sum=0
-    results[0]=0
+    current=''
     for row in reader:
-         results[p+1]=row["Profit/Losses"]
-         delta=float(results[p+1])-float(results[p])
-         if delta > max:
-          max=delta
-          pmax=row["Date"]
-         if delta < min:
-          min=delta
-          pmin=row["Date"] 
-         sum+=int(row["Profit/Losses"])
-         p+=1
-    dif=round((float(results[p])-float(results[1]))/(p-1),ndigits=2)
-    print("")   
+          if row["Candidate"] in results.keys():
+            results[row["Candidate"]] +=1        
+          if row["Candidate"] not in results.keys():
+           current=row["Candidate"]
+           results[current]=1
+    max_voters=max(results.values())           
     print(f'Total Voters: {reader.line_num-1}')
     print("---------------------------------------")
-    print(f'Total Amount: ${sum}')
-    print(f'Average Change: ${dif}')
-    print(f'Greatest Increase Change: {pmax} (${max})')
-    print(f'Greatest Decrease Change: {pmin} (${min})')
-        
-
-fu=open('analysis\pybank.txt', 'w')
-fu.write('Financial Analysis'+ '\n')
-fu.write("---------------------------------------"+ '\n')
-fu.write(f'Total Months: {reader.line_num-1}'+ '\n')
-fu.write(f'Total Amount: ${sum}'+ '\n')
-fu.write(f'Average Change: ${dif}'+ '\n')
-fu.write(f'Greatest Increase Change: {pmax} (${max})'+ '\n')
-fu.write(f'Greatest Decrease Change: {pmin} (${min})'+ '\n')
-fu.close()
+    print('')
+    for key, value in results.items():
+     print(f'{key}: {round(float(value*100/(reader.line_num-1)), ndigits=3)}% ({value})')
+     print('---------------------------------------')
+    print('')
+    inv_results={value: key for key, value in results.items()}
+    winner=inv_results[max_voters]
+    #print(winner)
+    print(f'Winner: {inv_results[max_voters]}')
+    print('---------------------------------------')
+    
+    fu=open('analysis\election.txt', 'w') 
+    with open('analysis\election.txt', 'w') as file:
+     fu.write('' '\n')
+     fu.write('Election Results'+ '\n')
+     fu.write('---------------------------------------'+ '\n')
+     fu.write(f'Total Voters: {reader.line_num-1}'+ '\n')
+     fu.write("---------------------------------------"+ '\n')
+     fu.write(''+ '\n')
+     for key, value in results.items():
+      zu=reader.line_num-1
+      vo=round(value*100/zu, ndigits=3)
+      fu.write(f'{key} {vo}%')
+      fu.write(f' ({value}) \n')
+     #fu.write(f'{key} {round(float(value*100/(reader.line_num-1)), ndigits=3)}% ({value}) \n')
+     fu.write("---------------------------------------"+ '\n')
+     fu.write(''+ '\n')
+     fu.write(f'Winner: {winner}'+ '\n')
+    fu.close()
